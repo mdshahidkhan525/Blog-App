0125d25b1ad88606022f43069469125913988858
shahid <shahid@gmail.com>
2023-10-09 18:32:55 +0530

    Added react router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 import Create from './components/create';
+import Read from './components/read';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="main">
-      <h2 className="main-header">React Crud Operations</h2>
-      <div>
-        <Create/>
+    <Router>
+      <div className="main">
+        <h2 className="main-header">React Crud Operations</h2>
+        <div>
+          <Routes>
+            <Route path='/create' element={<Create />}/>
+            <Route path='/read' element={<Read />}/>
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
