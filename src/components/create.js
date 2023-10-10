6cc87ec259027e478d990684c93f24106cd02059
shahid <shahid@gmail.com>
2023-10-10 11:27:51 +0530

    update actions

--- src/components/create.js
+++ src/components/create.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import { Button, Form } from 'semantic-ui-react'
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 export default function Create() {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
+  const navigate = useNavigate();
   const postData = () => {
    axios
       .post('http://localhost:3001/posts', {
@@ -13,6 +15,7 @@ export default function Create() {
       })
       .then(response => {
         console.log('Post created successful:', response.data);
+        navigate('/read');
       })
       .catch(error => {
         console.error('Error:', error);
--- src/components/read.js
+++ src/components/read.js
@@ -3,53 +3,60 @@ import { Button, Form, Table } from 'semantic-ui-react'
 import axios from 'axios';
 
 export default function Read() {
-  useEffect(() => {
-   axios
-    .get('http://localhost:3001/posts')
-    .then(response => {
-      setPosts(response.data)
-    })
-  }, [])
+  const [posts, setPosts] = useState([]);
 
-  const handleDelete = (postId) => {
-    axios
-       .delete(`http://localhost:3001/posts/${postId}`)
-       .then(response => {
-         console.log('Post deleted successfully', response.data);
-       })
-       .catch(error => {
-         console.error('Error:', error);
-       });
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const fetchPosts = () => {
+    axios.get('http://localhost:3001/posts')
+      .then(response => {
+        setPosts(response.data)
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
   }
 
-  const [posts, setPosts] = useState([])
-  console.log(posts, "posts")
-  return (
-        <div>
-            <Table singleLine>
-                <Table.Header>
-                    <Table.Row>
-                        <Table.HeaderCell>Title</Table.HeaderCell>
-                        <Table.HeaderCell>Description</Table.HeaderCell>
-                        <Table.HeaderCell>Action</Table.HeaderCell>
-                    </Table.Row>
-                </Table.Header>
+  const handleDelete = (postId) => {
+    axios.delete(`http://localhost:3001/posts/${postId}`)
+      .then(response => {
+        console.log('Post deleted successfully', response.data);
+        setPosts(prevPosts => prevPosts.filter(post => post.id !== postId));
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }
 
-                <Table.Body>
-                 { posts.map((post)=>{
-                   return (
-                    <Table.Row>
-                      <Table.Cell>{post.title}</Table.Cell>
-                      <Table.Cell>{post.body}</Table.Cell>
-                      <Table.Cell>
-                        <Button color="red" onClick={() => handleDelete(post.id)}>
-                          Delete Post
-                        </Button>
-                      </Table.Cell>
-                    </Table.Row>
-                  );})}
-                </Table.Body>
-            </Table>
-        </div>
+  return (
+    <div>
+      <Table singleLine>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Title</Table.HeaderCell>
+            <Table.HeaderCell>Description</Table.HeaderCell>
+            <Table.HeaderCell>Action</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          {posts.map((post) => {
+            return (
+              <Table.Row key={post.id}>
+                <Table.Cell>{post.title}</Table.Cell>
+                <Table.Cell>{post.body}</Table.Cell>
+                <Table.Cell>
+                  <Button color="red" onClick={() => handleDelete(post.id)}>
+                    Delete Post
+                  </Button>
+                </Table.Cell>
+              </Table.Row>
+            );
+          })}
+        </Table.Body>
+      </Table>
+    </div>
   )
 }
