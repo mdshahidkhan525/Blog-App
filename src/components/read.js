4c8deffc38766c8fd6c9bf5969934ae85630b842
shahid <shahid@gmail.com>
2023-10-11 12:38:51 +0530

    Added pagination

--- src/components/read.js
+++ src/components/read.js
@@ -1,16 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Form, Table, Modal } from 'semantic-ui-react'
+import { Button, Form, Table, Modal } from 'semantic-ui-react';
 import axios from 'axios';
 import Create from './create';
 import Edit from './edit';
 import SearchPost from './search_post';
+import ReactPaginate from 'react-paginate';
 
 export default function Read() {
   const [posts, setPosts] = useState([]);
   const [isCreateModalOpen, setIsCreateModalOpen] = useState(false);
   const [isEditModalOpen, setIsEditModalOpen] = useState(false);
   const [editPostId, setEditPostId] = useState(null);
-
+  const [currentPage, setCurrentPage] = useState(0);
+  const [postsPerPage] = useState(8);
 
   useEffect(() => {
     fetchPosts();
@@ -19,7 +21,7 @@ export default function Read() {
   const fetchPosts = () => {
     axios.get('http://localhost:3001/posts')
       .then(response => {
-        setPosts(response.data)
+        setPosts(response.data);
       })
       .catch(error => {
         console.error('Error:', error);
@@ -53,12 +55,16 @@ export default function Read() {
     setEditPostId(null);
   }
 
+  const indexOfLastPost = (currentPage + 1) * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
   return (
     <div>
       <div>
-        <SearchPost/>
+        <SearchPost />
         <Button color="green" onClick={() => newPost()}>
-           New Post
+          New Post
         </Button>
       </div>
       <Table singleLine>
@@ -72,7 +78,7 @@ export default function Read() {
         </Table.Header>
 
         <Table.Body>
-          {posts.map((post) => {
+          {currentPosts.map((post) => {
             return (
               <Table.Row key={post.id}>
                 <Table.Cell>{post.id}</Table.Cell>
@@ -96,7 +102,7 @@ export default function Read() {
       <Modal open={isCreateModalOpen} onClose={closeModal}>
         <Modal.Header>Add New Post</Modal.Header>
         <Modal.Content>
-          <Create setIsCreateModalOpen={setIsCreateModalOpen} fetchPosts={fetchPosts} closeModal={closeModal}/>
+          <Create setIsCreateModalOpen={setIsCreateModalOpen} fetchPosts={fetchPosts} closeModal={closeModal} />
         </Modal.Content>
       </Modal>
 
@@ -109,6 +115,22 @@ export default function Read() {
           </div>
         </Modal.Content>
       </Modal>
+
+      {/* Pagination Controls */}
+      <div className='pagination-center'>
+        <ReactPaginate
+          previousLabel={'previous'}
+          nextLabel={'next'}
+          breakLabel={'...'}
+          pageCount={Math.ceil(posts.length / postsPerPage)}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={5}
+          onPageChange={(selected) => setCurrentPage(selected.selected)}
+          containerClassName={'pagination'}
+          subContainerClassName={'pages pagination'}
+          activeClassName={'active'}
+        />
+      </div>
     </div>
-  )
+  );
 }
