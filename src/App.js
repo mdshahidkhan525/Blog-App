e73d5fb85ffeb9b028f503619533203df151a269
shahid <shahid@gmail.com>
2023-10-16 15:57:20 +0530

    Added component for login

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Create from './components/create';
 import Read from './components/read';
 import { BrowserRouter as Router, Route, Routes, Navigate, useNavigate } from 'react-router-dom'
 import SignUp from './components/users/signup';
+import SignIn from './components/users/signin';
 import { isUserLoggedIn } from './services/authService';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route path='/create' element={<Create />}/>
             <Route path='/' element={<Read />}/>
             <Route path='/signup' element={<SignUp />}/>
+            <Route path='/signin' element={<SignIn />}/>
           </Routes>
         </div>
       </div>
