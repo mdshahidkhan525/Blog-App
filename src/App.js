f7c8c4f61b35c224d93a0257f6c5670feadc5131
shahid <shahid@gmail.com>
2023-10-17 18:17:15 +0530

    add navbar links

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Create from './components/create';
 import Read from './components/read';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Link, Route } from 'react-router-dom';
 import SignUp from './components/users/SignUp';
 import SignIn from './components/users/signin';
 import { isUserLoggedIn } from './services/authService';
@@ -12,6 +12,17 @@ function App() {
       <div className="main">
         <h2 className="main-header">My Blog App</h2>
         <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+            <li>
+              <Link to="/signin">SignIn</Link>
+            </li>
+          </ul>
           <Routes>
             <Route path='create' element={<Create />}/>
             <Route path='/' element={<Read />}/>
